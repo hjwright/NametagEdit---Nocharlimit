81bfa53d54f2e690ee70491f0da8b4c20492ac01
sanjay900 <sanjay.govind9@gmail.com>
2014-03-20 23:32:18 +1300

    Fix PermissionsEX Group compatibility, possible fix client crashes
    Ive added checks to see if the permissions entity being changed is a
    group, and to get its players instead of just treating the group as a
    player.
    I tried to do this to GroupManager, but you can't get a list of group
    manager players from a group manager group.
    Ive removed removing all players from a team, and just deleting the
    team. The server was still stable over many reloads after i removed this
    code, and my clients no longer crashed.

--- src/main/java/ca/wacos/nametagedit/NametagHookPEX.java
+++ src/main/java/ca/wacos/nametagedit/NametagHookPEX.java
@@ -2,13 +2,23 @@ package ca.wacos.nametagedit;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import ru.tehkode.permissions.PermissionGroup;
+import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.events.PermissionEntityEvent;
 
 public class NametagHookPEX implements Listener {
 
 	@EventHandler
 	void onPermissionChangePEX(PermissionEntityEvent e) {
-
+		if (e.getEntity() instanceof PermissionGroup) {
+			PermissionGroup permGroup = (PermissionGroup) e.getEntity();
+			for (PermissionUser u: permGroup.getUsers()) {
+				String name = u.getName();
+				if (!NametagAPI.hasCustomNametag(name)) {
+					NametagAPI.resetNametag(name);
+				}	
+			}
+		} else
 		if (e.getEntity() != null) {
 			String name = e.getEntity().getName();
 			if (!NametagAPI.hasCustomNametag(name)) {
@@ -16,4 +26,5 @@ public class NametagHookPEX implements Listener {
 			}
 		}
 	}
+
 }
